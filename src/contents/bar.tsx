a70056deb52e71e862646bf4fc1b7379e2552011
davidh-lambda <142889449+davidh-lambda@users.noreply.github.com>
2023-11-15 17:44:17 +0100

    bar: init boxes collapsed

--- src/contents/bar.tsx
+++ src/contents/bar.tsx
@@ -54,7 +54,7 @@ const CollapsibleBox = (props) => {
 
   const toggleCollapse = () => setCollapsed(!collapsed);
 
-  if (collapsed)
+  if (!collapsed)
     return ( <div><h3 onClick={toggleCollapse}>{props.title} (click to close)</h3>{props.children}</div> );
 
   return ( <h3 onClick={toggleCollapse}>{props.title} (collapsed, click to open)</h3> );
