5ec5c3062a4f56fcda2600e824b7e0ec811e3771
davidh-lambda <142889449+davidh-lambda@users.noreply.github.com>
2023-11-16 10:28:50 +0100

    mini-feature: update plugin icon status on tab change

--- src/background/index.ts
+++ src/background/index.ts
@@ -25,3 +25,14 @@ chrome.action.onClicked.addListener(async (tab) => {
         text: newStatus ? "ON" : "OFF"
     });
 })
+
+// on tab change, update the plugin icon
+chrome.tabs.onActivated.addListener(activeInfo => {
+    chrome.tabs.get(activeInfo.tabId, async function(tab) {
+        const url = new URL(tab.url).hostname;
+        const activeURLs = (await storage.get("activeURLs")) || {}
+        chrome.action.setBadgeText({
+            text: activeURLs[url] ? "ON" : "OFF"
+        });
+    });
+})
