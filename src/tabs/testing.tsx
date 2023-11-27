ac1817abed0c7810dce2601625ff5e9830bb37e1
davidh-lambda <142889449+davidh-lambda@users.noreply.github.com>
2023-11-27 18:28:18 +0100

    tabs/testing: added

---
+++ src/tabs/testing.tsx
@@ -0,0 +1,15 @@
+
+function TestingPage() {
+    return (
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          padding: 16
+        }}>
+        <h2>Shaderunner Testing Page</h2>
+      </div>
+    )
+  }
+   
+  export default TestingPage;
