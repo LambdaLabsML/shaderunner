998b2b8db84aa00a03b988a6950a7cec8fb7cd35
davidh-lambda <142889449+davidh-lambda@users.noreply.github.com>
2023-12-01 10:20:47 +0100

    llm_classify: replaced class delimiter (,) with (|)

--- src/background/messages/llm_classify.ts
+++ src/background/messages/llm_classify.ts
@@ -6,36 +6,6 @@ import { HumanMessage, SystemMessage } from "langchain/dist/schema";
 const storage = new Storage()
 
 
-function splitStringIgnoringParentheses(input: any) {
-  let results = [];
-  let currentSegment = '';
-  let insideParentheses = false;
-
-  for (let char of input) {
-      if (char === '(') {
-          insideParentheses = true;
-      } else if (char === ')') {
-          insideParentheses = false;
-      }
-
-      if (char === ',' && !insideParentheses) {
-          results.push(currentSegment.trim());
-          currentSegment = '';
-      } else {
-          currentSegment += char;
-      }
-  }
-
-  // Add the last segment if it's not empty
-  if (currentSegment) {
-      results.push(currentSegment.trim());
-  }
-
-  return results;
-}
-
-
-
 function parseInput(input: string) {
     // Split the input into lines
     const lines = input.split('\n').filter(lines => lines.trim());
@@ -52,10 +22,10 @@ function parseInput(input: string) {
         return line;
       } else if (line.includes('Interesting Class Topics:')) {
         line = line.replace('Interesting Class Topics:', '').trim();
-        return line ? splitStringIgnoringParentheses(line).map(part => part.trim()) : [];
+        return line ? line.split("|") : [];
       } else if (line.includes('Outlier Class Topics:')) {
         line = line.replace('Outlier Class Topics:', '').trim();
-        return line ? splitStringIgnoringParentheses(line).map(part => part.trim()) : [];
+        return line ? line.split("|") : [];
       }
       return line.trim()
       
@@ -111,8 +81,8 @@ Title: LLM Performance Metrics and Improvements in the 2020s
 Query: performance improvements 2023
 Scope: middle (interesting are about 50% of the sentences)
 Thought: Classify sentences as 'interesting' if they are about enhancing or optimizing LLM performance. Classify as 'outliers' sentences that discuss broader topics or are irrelevant to performance improvements. As this is time-sensitive I should narrow down uninteresting years into the outlier list.
-Interesting Class Topics: Performance Enhancement, Optimization Strategies, Efficiency Increase
-Outlier Class Topics: year 2020, year 2021, year 2024, Applications, Use Cases, Design Challenges, Technical Specifications, User Testimonials, Industry Trends, Regulatory Considerations, Market Analysis, Cost-Effectiveness, Website Navigation, Advertisements, General News, External Links, Website Updates, Miscellaneous Announcements
+Interesting Class Topics: Performance Enhancement|Optimization Strategies|Efficiency Increase
+Outlier Class Topics: year 2020|year 2021|year 2024|Applications|Use Cases|Design Challenges|Technical Specifications|User Testimonials|Industry Trends|Regulatory Considerations|Market Analysis|Cost-Effectiveness|Website Navigation|Advertisements|General News|External Links|Website Updates|Miscellaneous Announcements
 
 # Training Exampple
 URL: www.myhealth.com/nutrition-tips
@@ -120,8 +90,8 @@ Title: Essential Nutrition Tips for a Healthy Lifestyle
 Query: Nutritional benefit types in vegetables
 Scope: broad (interesting are most sentences)
 Thought: The user is interested in understanding the nutritional benefits of vegetables. Therefore, sentences that directly discuss this topic are 'interesting'. The scope is broad, thus, related nutritional topics can also be considered 'interesting' to some extent. Since this is a broad scope I will list all nutricion benefit types that could match the request. Topics that are tangentially related to nutrition but not directly about the nutritional benefits of vegetables, or general health tips without specific mention of nutrition, would be classified as 'outliers'.
-Interesting Class Topics: Vitamins (A, C, K, and B-complex), Minerals (iron, calcium, potassium, magnesium), Dietary fiber, Antioxidants, Low calories, Low fat, Phytonutrients (flavonoids, carotenoids), Water content, Folate, Protein (in some vegetables like legumes)
-Outlier Class Topics: General Health Tips, Meat Nutrition, Fish Nutrition, Advertisements, Testimonials, External Links
+Interesting Class Topics: Vitamins (A, C, K, and B-complex)|Minerals (iron, calcium, potassium, magnesium)|Dietary fiber|Antioxidants|Low calories|Low fat|Phytonutrients (flavonoids, carotenoids)|Water content|Folate|Protein (in some vegetables like legumes)
+Outlier Class Topics: General Health Tips|Meat Nutrition|Fish Nutrition|Advertisements|Testimonials|External Links
 
 # Training Example
 URL: www.cppalgorithms.com/Rabin-Karp-algorithm
@@ -129,17 +99,17 @@ Title: Rabin-Karp string search algorithm
 Query: Core Idea
 Scope: narrow (interesting are only few sentences)
 Thought: Since the page is about the Rabin-Karp algorithm and the user asks for it's core idea, I need to think around the corner to highlight the specific sentences. In this case I should reference hashing and string matching. At the same time I add close specific topics to the negative class to compensate for false positives.
-Interesting Class Topics: Core Idea and Trick (Rolling Hash Mechanism), Implementation of the Rabin-Karp Algorithm
-Outlier Class Topics: Hashing, String Matching, Algorithm History, Algorithm Theory, String Matching Algorithms, Usage, Related Algorithms, Code Expression, Programming in C++, Site Navigation, External Links
+Interesting Class Topics: Core Idea and Trick (Rolling Hash Mechanism)|Implementation of the Rabin-Karp Algorithm
+Outlier Class Topics: Hashing|String Matching|Algorithm History|Algorithm Theory|String Matching Algorithms|Usage|Related Algorithms|Code Expression|Programming in C++|Site Navigation|External Links
 
 # Training Example
 URL: www.spaceexplorationnews.com/moon-landing
 Title: The History of Moon Landings
 Query: Source of "The Eagle has landed" quote
 Scope: super-narrow (looking for specific information)
-Thought: The user is specifically looking for the source of the quote "The Eagle has landed". Sentences that directly discuss this quote, its context, or its origin are 'interesting'. The focus should be extremely narrow, centered on the quote and its immediate context.
-Interesting Class Topics: "The Eagle has landed", Apollo 11 Moon Landing Quote
-Outlier Class Topics: General Moon Landing History, Other Space Missions, Astronaut Biographies, Space Exploration Technology, Future Moon Missions, Advertisements, External Links, Site Navigation
+Thought: The user is specifically looking for the source of the quote "The Eagle has landed". Sentences that directly discuss this quote|its context|or its origin are 'interesting'. The focus should be extremely narrow|centered on the quote and its immediate context.
+Interesting Class Topics: "The Eagle has landed"|Apollo 11 Moon Landing Quote
+Outlier Class Topics: General Moon Landing History|Other Space Missions|Astronaut Biographies|Space Exploration Technology|Future Moon Missions|Advertisements|External Links|Site Navigation
 `
 
     const USER = `# Incoming User Request (always use training example template)
