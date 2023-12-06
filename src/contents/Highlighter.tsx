1add989a34fd517399306721274e0626760c1005
davidh-lambda <142889449+davidh-lambda@users.noreply.github.com>
2023-12-06 20:49:54 +0100

    Highlighter: smoothened status_highlighter by increasing batchSize in "recompute" mode

--- src/contents/Highlighter.tsx
+++ src/contents/Highlighter.tsx
@@ -151,7 +151,7 @@ const Highlighter = () => {
 
       // retrieve embedding (either all at once or batch-wise)
       let splitEmbeddings = {};
-      const batchSize = 32;
+      const batchSize = exists ? 256 : 64;
       for(let i = 0; i < splits.length; i+= batchSize) {
         const splitEmbeddingsBatch = await computeEmbeddingsCached(url as string, splits.slice(i, i+batchSize))
         splitEmbeddings = {...splitEmbeddings, ...splitEmbeddingsBatch};
